0660a1f35630ef66eaa0b4689c274107a6caf956
charlchua-ciit <charl.chua@ciit.edu.ph>
2024-03-15 15:39:43 +0800

    aesthetic adjustment

--- FinalProjectphp/footer.php
+++ FinalProjectphp/footer.php
@@ -1,7 +1,7 @@
 <div class="spacer"></div>
 <!-- Footer-->
-<footer class="py-5 bg-dark fixed-bottom footer">
-            <div class="container"><p class="m-0 text-center text-green">Copyright &copy; Your Website 2023</p></div>
+<footer class="py-4 bg-dark fixed-bottom footer">
+            <div class="container"><p class="m-0 text-center text-green">Copyright &copy; RetroStop 2024</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
